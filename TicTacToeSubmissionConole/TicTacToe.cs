609bf5f0683b85690e239e48f09792627c47c607
Tarryn-M20 <102674265+Tarryn-M20@users.noreply.github.com>
2022-04-06 10:59:03 +0200

    Add project files.

---
+++ TicTacToeSubmissionConole/TicTacToe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToeRendererLib.Enums;
+using TicTacToeRendererLib.Renderer;
+
+namespace TicTacToeSubmissionConole
+{
+    public class TicTacToe
+    {
+        private TicTacToeConsoleRenderer _boardRenderer; // lives throughout this class (_bboardRenderer)
+
+        public TicTacToe()
+        {
+            _boardRenderer = new TicTacToeConsoleRenderer(10,6);
+            _boardRenderer.Render();
+            // render means show to screen 
+        }
+
+
+        public void Run() // methord returns nothing. and reason its public is so it can be accessed outside class
+        {
+
+            // FOR ILLUSTRATION CHANGE TO YOUR OWN LOGIC TO DO TIC TAC TOE
+
+            Console.SetCursorPosition(2, 19);
+
+            Console.Write("Player X");
+
+            Console.SetCursorPosition(2, 20);
+
+            Console.Write("Please Enter Row: ");
+            var row = Console.ReadLine();
+
+            Console.SetCursorPosition(2, 22);
+
+
+            Console.Write("Please Enter Column: ");
+            var column = Console.ReadLine();
+
+
+            // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
+            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);            
+
+        }
+
+    }
+}
